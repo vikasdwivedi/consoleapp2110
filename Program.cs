53a513931ecdb150c48c5b1a5c3cf14633700cd1
Vikas Dwivedi <vik.dwivedi30@gmail.com>
2021-10-21 12:10:20 +0530

    first commit
    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace consoleapp2110
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Users...from dev!");
+        }
+    }
+}
